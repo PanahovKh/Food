584514833189220b598868a9cddb49144304019d
panahovkh <xanali.panahov1994@gmail.com>
2020-08-18 23:30:02 +0400

    adding timer  and modal page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+    //Tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items')
@@ -37,30 +38,77 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    //Timer
+    const deadline = '2020-08-30';
 
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor( (t/(1000*60*60*24)) ),
+            seconds = Math.floor( (t/1000) % 60 ),
+            minutes = Math.floor( (t/1000/60) % 60 ),
+            hours = Math.floor( (t/(1000*60*60) % 24) );
 
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 
+    function getZero(num){
+        if (num >= 0 && num < 10) { 
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
 
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
 
+        updateClock();
 
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
 
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
 
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
 
+    setClock('.timer', deadline);
 
+    //Modal
 
+    const modalTrigger = document.querySelector('[data-modal]'),
+    modal = document.querySelector('.modal'),   
+    modalCloseBtn = document.querySelector('[data-close]');
 
+    modalTrigger.addEventListener('click', () => {
+        // modal.classList.add('show');
+        // modal.classList.remove('hide');
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
+    modalCloseBtn.addEventListener('click', () => {
+        // modal.classList.add('hide');
+        // modal.classList.remove('show');
+        modal.classList.toggle('show');
+        document.body.style.overflow = '';
+    });
 
 });
